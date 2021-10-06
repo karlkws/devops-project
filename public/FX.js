091a9fa4f9eb8ba98fa5757b5c8dfac44d9e831a
Karl <karlkws95@gmail.com>
2021-10-06 20:02:39 +0800

    updated app fx date limit

--- public/FX.js
+++ public/FX.js
@@ -6,19 +6,19 @@ Source : Polygon.io
 
 var today = new Date();
 today.setDate(today.getDate() - 1);
-// var dd = today.getDate();
-// var mm = today.getMonth() + 1; //January is 0!
-// var yyyy = today.getFullYear();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
 
-// if (dd < 10) {
-//    dd = '0' + dd;
-// }
+if (dd < 10) {
+   dd = '0' + dd;
+}
 
-// if (mm < 10) {
-//    mm = '0' + mm;
-// } 
+if (mm < 10) {
+   mm = '0' + mm;
+} 
     
-// today = yyyy + '-' + mm + '-' + dd;
+today = yyyy + '-' + mm + '-' + dd;
 document.getElementById("transaction").setAttribute("max", today);
 
 
